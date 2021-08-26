cff8722e380709dae16a0232ac92a9e27627062a
Pedro Nunes <pnunes91@gmail.com>
2021-08-26 12:18:37 -0300

    signup working.
    got basic signup working. now I must send this to the db for validation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -8,13 +8,25 @@ import {
   Button,
   FormControl,
   makeStyles,
-  CardHeader
+  CardHeader,
+  ThemeProvider
 } from '@material-ui/core'
 
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    
+    paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(2),
+
+  },
+  password: {
+    marginTop: theme.spacing(2)
+  },
+  button: {
+    justifyContent:'center',
+  },
+  title: {
+    textAlign: 'center'
   }
 }))
 
@@ -32,43 +44,33 @@ export default function Signup(props) {
 
 
   const handleUsernameUpdate = (e) => {
-    if (e.target.value.length <= 4) {
-      setUserErrorText("* must have at least 4 characters")
+    if (e.target.value.length < 4) {
+      promptUsernameError("* must have at least 4 characters")
     }
     else {
-      setUsername(e.target.value);
+      promptUsernameError("");
     }
-  }
-
-  const promptUsernameError = () => {
-    setUserErrorText("* username already exists")
-  }
-
-  const promptPasswordError = () => {
-    setPasswordErrorText("* must match")
-  }
-
-  const promptPasswordConfirmError = () => {
-    setPasswordConfirmErrorText("* must match")
+    setUsername(e.target.value);
   }
 
   const handlePasswordUpdate = (e) => {
-    if (e.target.value.length <= 4) {
-      console.log('password too small' + e);
+    if (e.target.value.length < 6) {
+      promptPasswordError("* must have at least 6 characters")
     }
     else {
-      setPassword(e.target.value);
+      promptPasswordError("")
     }
-
+    setPassword(e.target.value);
   }
 
   const handlePasswordConfirmUpdate = (e) => {
-    if (e.target.value.length <= 4) {
-      console.log('password confirm too small' + e);
+    if (e.target.value.length < 6) {
+      promptPasswordConfirmError("* must have at least 6 characters")
     }
     else {
-      setPasswordConfirm(e.target.value);
+      promptPasswordConfirmError("")
     }
+    setPasswordConfirm(e.target.value);
   }
 
   const handleSubmit = (e) => {
@@ -78,19 +80,45 @@ export default function Signup(props) {
     //  setPassword("");
     //  setPasswordConfirm("");
     // }
-    if (password !== passwordConfirm) {
-      promptPasswordError(); 
-      promptPasswordConfirmError();
+    let isValid = true;
+    if (username.length < 4) {
+      promptUsernameError("* must have at least 4 characters")
+      isValid = false;
     }
-    else {
+
+    if (password.length < 6) {
+      promptPasswordError("* must have at least 6 characters")
+      isValid = false;
+    }
+
+    if (password !== passwordConfirm) {
+      promptPasswordConfirmError('* passwords must match');
+      isValid = false;
+    }
+
+    if(isValid) {
       //submit stuff to firebase db
       console.log("submit stuff to firebase db")
     }
   }
 
+  
+  const promptUsernameError = (message) => {
+    setUserErrorText(message)
+  }
+
+  const promptPasswordError = (message) => {
+    setPasswordErrorText(message)
+  }
+
+  const promptPasswordConfirmError = (message) => {
+    setPasswordConfirmErrorText(message)
+  }
+
+
   return (
     <Card aria-labelledby="form-dialog-title" maxWidth="xs" className={classes.root} fullWidth >
-      <CardHeader title="Sign Up" />
+      <CardHeader title="Sign Up" className={classes.title} />
       <form onSubmit={handleSubmit}>
         <CardContent >
           <FormControl required fullWidth>
@@ -98,50 +126,55 @@ export default function Signup(props) {
               required
               autoFocus
               autoComplete="false"
-              id="task-desc"
+              id="username"
               label="Username"
               variant="outlined"
+              value={username}
               error={(userErrorText === "" ? false : true )}
-              errorText={userErrorText}
+              helperText={userErrorText}
               onChange={e => handleUsernameUpdate(e)}
             />
           </FormControl>
         </CardContent>
-        <CardContent className={classes.dialogContent}>
+
+        <CardContent className={classes.password}>
           <FormControl required fullWidth>
             <TextField
               required
               autoFocus
               autoComplete="false"
-              id="task-desc"
-              label="Pasword"
+              id="pwd"
+              label="Password"
               type="password"
-              helperText="at least 6 characters"
               variant="outlined"
+              value={password}
               error={(passwordErrorText === "" ? false : true )}
-              errorText={passwordErrorText}
+              helperText={passwordErrorText}
               onChange={e => handlePasswordUpdate(e)}
             />
           </FormControl>
+          </CardContent>
 
+          <CardContent >
           <FormControl required fullWidth>
             <TextField
               required
               autoFocus
               autoComplete="false"
-              id="task-desc"
+              id="pwd-confirm"
               label="Confirm Password"
               type="password"
               variant="outlined"
+              value={passwordConfirm}
               error={(passwordConfirmErrorText === "" ? false : true )}
-              errorText={passwordConfirmErrorText}
+              helperText={passwordConfirmErrorText}
               onChange={e => handlePasswordConfirmUpdate(e)}
             />
           </FormControl>
 
         </CardContent>
 
-        <CardActions className={classes.dialogActionBar}>
+        <CardActions className={classes.button}>
           <Button type="submit" color="primary" size="large" variant="contained" >
             Sign Up
           </Button>
